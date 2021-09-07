9ecd48df5cd4306dc8086e542025ec00b0177d7d
Nathan Lafarge <lafarge.nathan3b3@gmail.com>
2021-09-07 16:00:29 +0200

    sort hundred

--- srcs/sorters/ft_sort_hundred.c
+++ srcs/sorters/ft_sort_hundred.c
@@ -6,7 +6,7 @@
 /*   By: nathanlafarge <nathanlafarge@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 01:44:07 by nlafarge          #+#    #+#             */
-/*   Updated: 2021/09/07 02:20:10 by nathanlafar      ###   ########.fr       */
+/*   Updated: 2021/09/07 02:25:02 by nathanlafar      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_sort_hundred(t_ps *ps, int chuncks)
 {
 	int	range;
 
-	(void)chuncks;
-	(void)range;
 	ft_create_sorted_list(ps);
+	range = ps->tab_a.size / chuncks;
+	printf("%d", range);
 }
