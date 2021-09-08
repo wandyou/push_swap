0eceb71132d0e3145c47be7f5a1038cb2540f0ca
Nathan LAFARGE <nlafarge@e1r11p11.42.fr>
2021-09-09 01:48:36 +0200

    find first

--- srcs/sorters/ft_sort_hundred.c
+++ srcs/sorters/ft_sort_hundred.c
@@ -6,11 +6,26 @@
 /*   By: nlafarge <nlafarge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 01:44:07 by nlafarge          #+#    #+#             */
-/*   Updated: 2021/09/09 00:54:11 by nlafarge         ###   ########.fr       */
+/*   Updated: 2021/09/09 01:48:25 by nlafarge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
+
+void	ft_find_first(t_ps *ps)
+{
+	int	i;
+	int pos;
+	int mediane;
+	
+	i = 0;
+	mediane = ps->tab_a.size / 2;
+	while (i < ps->tab_a.size)
+	{
+		if (ps->tab_a.nums[i] >= ps->chunck.min && ps->tab_a.nums[i] <= ps->chunck.max)
+			pos = i;
+	}
+}
 
 void	ft_init_chunck_info(t_ps *ps, int chuncks)
 {
@@ -19,7 +34,7 @@ void	ft_init_chunck_info(t_ps *ps, int chuncks)
 	{
 		ps->chunck.size = ps->nb_nums - ((chuncks - 1) * ps->chunck.size);
 		printf("Chunck size : %d\n", ps->chunck.size);
-		ps->chunck.min = ps->sorted[ps->nb_nums - ps->chunck.size - 1];
+		ps->chunck.min = ps->sorted[ps->nb_nums - ps->chunck.size];
 		printf("Chunck min : %d\n", ps->chunck.min);
 		ps->chunck.max = ps->sorted[ps->nb_nums - 1];
 		printf("Chunck max : %d\n\n", ps->chunck.max);
@@ -32,7 +47,6 @@ void	ft_init_chunck_info(t_ps *ps, int chuncks)
 		ps->chunck.max = ps->sorted[(ps->chunck.size * ps->chunck.i) + ps->chunck.size - 1];
 		printf("Chunck max : %d\n\n", ps->chunck.max);
 	}
-	//printf("%d\n%d\n%d\n%d\n\n", ps->chunck.i, ps->chunck.size, ps->chunck.min, ps->chunck.max);
 }
 
 void	ft_sort_hundred(t_ps *ps, int chuncks)
@@ -45,20 +59,15 @@ void	ft_sort_hundred(t_ps *ps, int chuncks)
 	chunck_count = 0;
 	ps->chunck.size = ps->nb_nums / chuncks;
 	ps->chunck.i = 0;
-	//printf("%d\n\n", ps->sorted[ps->chunck.size * 5]);
 	while (i < ps->nb_nums)
 	{
-		printf("Number : %d\n", ps->sorted[i]);
 		if (chunck_count == ps->chunck.size && ps->chunck.i < 5)
 		{
 			chunck_count = 0;
 			ps->chunck.i++;
 		}
-		// ps->chunck.i = i / ps->chunck.size;
-		// if (ps->chunck.i > 5)
-		// 	ps->chunck.i = 5;
-		printf("chunck i : %d\n", ps->chunck.i);
 		ft_init_chunck_info(ps, chuncks);
+		ft_find_first(ps);
 		i++;
 		chunck_count++;
 	}
